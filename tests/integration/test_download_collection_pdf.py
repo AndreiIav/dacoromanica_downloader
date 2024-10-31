9474105a37f600c7e0e6a16117dc4d32565efe3f
Your Name <you@example.com>
2024-10-31 10:18:18 +0200

    Added tests.

--- tests/integration/test_download_collection_pdf.py
+++ tests/integration/test_download_collection_pdf.py
@@ -9,7 +9,7 @@ from dacoromanica_downloader.download_pdf import (
 
 
 @pytest.mark.parametrize("test_file", ["test.pdf"])
-def test_download_collection_pdf(
+def test_download_collection_pdf_saves_file(
     access_local_file_with_requests, get_path_to_test_file, tmp_path, capsys
 ):
     pdf_link = get_path_to_test_file
@@ -33,9 +33,32 @@ def test_download_collection_pdf(
     assert f"'{pdf_name}' downloaded in '{destination_folder}' folder." in out
 
 
-# TODO
-def test_download_collection_pdf_does_not_download_file_if_http_exception_is_raised():
-    pass
+def test_download_collection_pdf_does_not_download_file_if_no_valid_requests_response(
+    tmp_path, capsys
+):
+    pdf_link = "pdf_link"
+    pdf_name = "test_pdf_name.pdf"
+    destination_folder = tmp_path
+
+    def get_request_returns_error_message(link):
+        return "Error message"
+
+    http_response = get_request_returns_error_message(pdf_link)
+
+    download_collection_pdf(
+        pdf_link=pdf_link,
+        pdf_name=pdf_name,
+        destination_folder=destination_folder,
+        fn_get_response=get_request_returns_error_message,
+    )
+
+    destination_path = tmp_path / pdf_name
+    out, _ = capsys.readouterr()
+
+    assert not destination_path.is_file()
+    assert (
+        f"'{pdf_name}' was not downloaded due to this error: {http_response} ."
+    ) in out
 
 
 @pytest.mark.parametrize("test_file", ["test.pdf"])
@@ -72,18 +95,74 @@ def test_download_collection_pdf_shortens_pdf_name_that_is_over_limit_and_saves_
 
     assert destination_path.is_file()
     assert (
-        f"'{pdf_name}' file name was shortened to: '{pdf_name_within_limit}{extension}'"
+        f"'{pdf_name}' file name was shortened to:"
+        f" '{pdf_name_within_limit}{extension}'"
     ) in out
     assert (
-        f"'{pdf_name_within_limit}{extension}' downloaded in '{destination_folder}' folder."
-        in out
+        f"'{pdf_name_within_limit}{extension}' downloaded in"
+        f" '{destination_folder}' folder."
+    ) in out
+
+
+@pytest.mark.parametrize("test_file", ["test.pdf"])
+def test_download_collection_pdf_does_not_download_file_if_name_cannot_be_shortened(
+    access_local_file_with_requests, get_path_to_test_file, tmp_path, capsys
+):
+    pdf_link = get_path_to_test_file
+    destination_folder = tmp_path
+    destination_folder_string_length = len(str(destination_folder))
+    filename_limit = destination_folder_string_length - 10
+    pdf_name = "a.pdf"
+
+    new_fn_get_link_response = partial(
+        get_link_response, get_request=access_local_file_with_requests
     )
 
+    download_collection_pdf(
+        pdf_link=pdf_link,
+        pdf_name=pdf_name,
+        destination_folder=destination_folder,
+        fn_get_response=new_fn_get_link_response,
+        path_length_limit=filename_limit,
+    )
 
-# TODO
-def test_download_collection_pdf_does_not_download_file_if_name_cannot_be_shortened():
-    pass
+    destination_path = tmp_path / pdf_name
+    out, _ = capsys.readouterr()
+
+    assert (
+        f"'{destination_path}' file name is too long and cannot be saved."
+        " File not downloaded."
+    ) in out
+    assert not destination_path.is_file()
 
 
-def test_download_collection_pdf_does_not_download_file_if_it_is_already_downloaded():
-    pass
+@pytest.mark.parametrize("test_file", ["test.pdf"])
+def test_download_collection_pdf_does_not_download_file_if_it_is_already_downloaded(
+    access_local_file_with_requests, get_path_to_test_file, tmp_path, capsys
+):
+    destination_folder = tmp_path
+    already_existing_file = destination_folder / "test_pdf_name.pdf"
+    already_existing_file_content = b"Some content"
+    already_existing_file.write_bytes(already_existing_file_content)
+
+    pdf_link = get_path_to_test_file
+    pdf_name = "test_pdf_name.pdf"
+    new_fn_get_link_response = partial(
+        get_link_response, get_request=access_local_file_with_requests
+    )
+
+    download_collection_pdf(
+        pdf_link=pdf_link,
+        pdf_name=pdf_name,
+        destination_folder=destination_folder,
+        fn_get_response=new_fn_get_link_response,
+    )
+
+    out, _ = capsys.readouterr()
+    assert (
+        f"'{pdf_name}' already present in '{destination_folder}' folder"
+        " so it will not be downloaded."
+    ) in out
+
+    file_content = (destination_folder / pdf_name).read_bytes()
+    assert file_content == already_existing_file_content
