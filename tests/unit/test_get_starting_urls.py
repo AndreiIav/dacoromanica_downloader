cbdd01670378a7e6365d6aec45ecb7e3bc259046
Your Name <you@example.com>
2024-11-12 10:07:03 +0200

    Added tests for get_starting_urls().

---
+++ tests/unit/test_get_starting_urls.py
@@ -0,0 +1,32 @@
+import pytest
+
+from dacoromanica_downloader.get_starting_urls import EmptyFileError, get_starting_urls
+
+
+def test_get_starting_urls_gets_urls(tmp_path):
+    file_location = tmp_path / "test_file_urls.txt"
+    with open(file_location, "a", encoding="utf_8") as f:
+        f.write("link_1\n")
+        f.write("link_2 ")
+        f.write("link_3")
+
+    res = get_starting_urls(file_location)
+
+    assert res == ["link_1", "link_2", "link_3"]
+
+
+def test_get_starting_urls_raises_FileNotFoundError(tmp_path):
+    with pytest.raises(FileNotFoundError) as err:
+        get_starting_urls(tmp_path)
+
+    assert str(err.value) == f"{tmp_path} file does not exists."
+
+
+def test_get_starting_urls_raises_EmptyFileError(tmp_path):
+    file_location = tmp_path / "test_file_urls.txt"
+    file_location.touch()
+
+    with pytest.raises(EmptyFileError) as err:
+        get_starting_urls(file_location)
+
+    assert str(err.value) == f"'{file_location}' file contains no data."
