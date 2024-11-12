ba0a8ee5a53c58c423d9c726640ac65f57665b07
Your Name <you@example.com>
2024-11-12 16:06:11 +0200

    Changed name of test_case from test_get_page_collections to test_create_CollectionPdf and completely refactored it.

--- tests/end-to-end/test_main.py
+++ tests/end-to-end/test_main.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from pathlib import Path
 
 import pytest
@@ -5,11 +6,12 @@ import requests
 
 from dacoromanica_downloader.download_pdf import get_link_response
 from dacoromanica_downloader.main import (
+    create_CollectionPdf,
     generate_next_page_url,
-    get_page_collections,
     main,
 )
-from dacoromanica_downloader.scrape import get_collection_info, get_soup
+from dacoromanica_downloader.model import CollectionPdf
+from dacoromanica_downloader.scrape import get_soup
 
 
 @pytest.mark.parametrize("test_file", ["test_data_main/collections_page1.html"])
@@ -26,18 +28,33 @@ def test_generate_next_page_url(get_path_to_test_file, access_local_file_with_re
     assert res_list[0] == "collection_details_page1.html"
 
 
-@pytest.mark.parametrize("test_file", ["test_data_main/table_view_collections1.html"])
-def test_get_page_collections(get_path_to_test_file, access_local_file_with_requests):
-    link = get_path_to_test_file
-    response = get_link_response(link, access_local_file_with_requests)
-    soup = get_soup(response)
-    all_collections_on_page_details = get_collection_info(
-        soup, collections_base_link_identifier="collection_details"
+def test_create_CollectionPdf():
+    CollectionInfo = namedtuple(
+        "CollectionInfo", ["details_link", "title", "author", "pdf_link"]
+    )
+    collection_information = [
+        ["details_link_1", "title_1", "author_1", "pdf_link_1"],
+        ["details_link_2", "title_2", "author_2", "pdf_link_2"],
+    ]
+    generator = (
+        CollectionInfo(details_link=i[0], title=i[1], author=i[2], pdf_link=i[3])
+        for i in collection_information
     )
 
-    res = get_page_collections(all_collections_on_page_details)
+    res = create_CollectionPdf(generator)
 
-    assert len(res) == 2
+    assert isinstance(res[0], CollectionPdf)
+    assert isinstance(res[1], CollectionPdf)
+
+    assert res[0].details_link == "details_link_1"
+    assert res[0].title == "title_1"
+    assert res[0].author == "author_1"
+    assert res[0].pdf_link == "pdf_link_1"
+
+    assert res[1].details_link == "details_link_2"
+    assert res[1].title == "title_2"
+    assert res[1].author == "author_2"
+    assert res[1].pdf_link == "pdf_link_2"
 
 
 @pytest.mark.parametrize("test_file", ["test_data_main/collections_page1.html"])
